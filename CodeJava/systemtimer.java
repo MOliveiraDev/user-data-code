f38301304fd59eecf4ed5b1ba79873fc1d154ca8
Oliver.dev <oliveiramaicon16637@gmail.com>
2024-10-02 01:48:56 -0300

    Create systemtimer.java

---
+++ CodeJava/systemtimer.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class systemtimer {
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("Este código mostra o horário, data, mês e o ano ");
+        System.out.println(relogio.toString());
+    }
+}
